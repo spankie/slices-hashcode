6c5e35e91a7e559a7fd18a5165cb85a61acaa55b
Odohi David <odohi.david@gmail.com>
2020-01-18 01:01:01 +0100

    some refactoring on the code

--- main.go
+++ main.go
@@ -5,48 +5,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-var files = [...]string{
-	"inputs/a_example.in",
-	"inputs/b_small.in",
-	"inputs/c_medium.in",
-	"inputs/d_quite_big.in",
-	"inputs/e_also_big.in",
-}
-
 func main() {
-	for ix := range files {
-		file, err := os.Open(files[ix])
-		if err != nil {
-			log.Fatalf("failed to open file: %s", err)
-			os.Exit(3)
-		}
-
-		scanner := bufio.NewScanner(file)
-		scanner.Split(bufio.ScanLines)
-
-		var lines []string
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
-		}
-		file.Close()
-
-		maxAndNo := extract(strings.Split(lines[0], " "))
-		sliceNos := extract(strings.Split(lines[1], " "))
-
-		// Map to store the pizza types
-		types := make(map[int]int)
-		for ix, val := range *sliceNos {
-			types[ix] = val
-		}
-
-		count, pizzasAdded := simulate(maxAndNo, sliceNos, &types)
-		out(count, pizzasAdded, files[ix])
+	// create output directory
+	err := os.Mkdir("outputs", os.ModePerm)
+	if err != nil {
+		fmt.Printf("Could not create outputs directory: %v\n", err)
 	}
+	// Read the `input` directory so that we don't have to
+	// modify the code whenever we want to test other inputs
+	_ = filepath.Walk("inputs", func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			// for ix := range files {
+			file, err := os.Open(path)
+			if err != nil {
+				// we dont want to alter the whole code if just one file does not open
+				return err
+				// log.Fatalf("failed to open file: %s", err)
+				// os.Exit(3)
+			}
+
+			scanner := bufio.NewScanner(file)
+			scanner.Split(bufio.ScanLines)
+
+			var lines []string
+			for scanner.Scan() {
+				lines = append(lines, scanner.Text())
+			}
+			file.Close()
+
+			maxAndNo := extract(strings.Split(lines[0], " "))
+			sliceNos := extract(strings.Split(lines[1], " "))
+
+			// Map to store the pizza types
+			types := make(map[int]int)
+			for ix, val := range *sliceNos {
+				types[ix] = val
+			}
+
+			pizzasAdded := simulate(maxAndNo, sliceNos, &types)
+			out(pizzasAdded, path)
+			// }
+		}
+		return nil
+	})
+	// fmt.Printf("Error reading the input directory: %v", err)
 }
 
 // extract returns the integer equivalents of numbers  on each line of the input file...translated into a slice
@@ -69,68 +77,53 @@ func extract(slice []string) *[]int {
 // and then adds from the first element whilst checking if the accumulated total isn't
 // More than the given maximum constraint, and then returns the numbers of
 // Different pizzas to order and which types to order.
-func simulate(maxNo *[]int, slices *[]int, types *map[int]int) (*int, *[]int) {
+func simulate(maxNo *[]int, slices *[]int, types *map[int]int) *[]int {
 
 	maxAndNo := *maxNo
 	sliceNos := *slices
 	max := maxAndNo[0]
 
-	sort.Slice(sliceNos, func(i, j int) bool {
-		return sliceNos[i] > sliceNos[j]
-	})
-
 	total := 0
-	count := 0
-	var pizzaSlice []int
-	for _, val := range sliceNos {
-		if (total + val) <= max {
-			total += val
-			pizzaSlice = append(pizzaSlice, val)
-			count++
-		}
-	}
-	fmt.Println("--------------------------------")
-	fmt.Println("--------------------------------")
-	fmt.Println("The total is ", total)
-	fmt.Println()
-
-	// pizzatype is the map of of the original pizza to their respective number of slices
-	// pizzasAdded is a slice that'll hold the different types of pizzas added
-	// (remember the pizzas are named with number e.g tyep1,type2 etc..)
-	pizzatype := *types
+	// var pizzaSlice []int
 	var pizzasAdded []int
 
-	// addedSlice holds the current pizza Slice
-	for _, addedSlice := range pizzaSlice {
-	loop:
-		for key, val := range pizzatype {
-			if addedSlice == val {
-				pizzasAdded = append(pizzasAdded, key)
-				break loop
-			}
+	// using this to loop through the slice from the back
+	// this is because the larger values are at the end of the slice...
+	// from the example file we have [2 5 6 8] right, the larger numbers are at the end of the slice
+	for key := len(sliceNos) - 1; key >= 0; key-- {
+		val := sliceNos[key]
+		if (total + val) <= max {
+			total += val
+			// pizzaSlice = append(pizzaSlice, val)
+			pizzasAdded = append(pizzasAdded, key)
 		}
 	}
+	fmt.Println("The total is ", total)
 
 	// Now because the output requires that the kinds of pizzas we buy to be listed in ascending order...i sort the pizzasAdded
 	// slice in ascending order
 	sort.Slice(pizzasAdded, func(i, j int) bool {
 		return pizzasAdded[i] < pizzasAdded[j]
 	})
-	return &count, &pizzasAdded
+	return &pizzasAdded
 }
 
 // out will write out output to files, named relative to the input file's name
-func out(count *int, types *[]int, filename string) {
-	filename = strings.TrimPrefix(filename, "inputs/")
-	filename = strings.TrimSuffix(filename, ".in")
-	filename = filename + "_output"
+func out(types *[]int, filename string) {
+	// replace all occurence of "in" with "out"
+	filename = strings.Replace(filename, "in", "out", -1)
 
-	fmt.Println(filename)
-	fmt.Println(*count)
-	fmt.Println(*types)
-	fmt.Println("--------------------------------")
-	fmt.Println("--------------------------------")
-	fmt.Print("test")
-	fmt.Print("test1")
-
+	fmt.Println("Writing answer to file: ", filename)
+	fmt.Println(len(*types), " types of pizza")
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Cannot create file for saving result: ", err)
+	}
+	_, err = f.Write([]byte(strconv.Itoa(len(*types)) + "\n" + strings.Trim(fmt.Sprint(types), "&[]")))
+	if err != nil {
+		fmt.Println("Cannot write result to file: ", err)
+	}
+	f.Sync()
+	f.Close()
+	fmt.Println()
 }
